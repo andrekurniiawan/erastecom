bff1b08e924117ee5b59734676a61a0a354d5a03
Andre Kurniawan <andrekurniiawan@gmail.com>
2020-05-20 23:05:26 +0700

    navbar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('product', 'ProductController');
 Route::resource('order', 'OrderController');
+Route::resource('user', 'UserController');
 
 Auth::routes();
 
