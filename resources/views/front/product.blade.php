dc1262510d20a3fe275629faec652b5d0aaa9269
Andre Kurniawan <andrekurniiawan@gmail.com>
2020-05-21 18:30:21 +0700

    front card columns

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -5,15 +5,17 @@
   <div class="title">
     <h1>Products</h1>
   </div>
-  @foreach ($products as $product)
-  <div class="card" style="width: 18rem;">
-    <img src="https://via.placeholder.com/300" class="card-img-top" alt="">
-    <div class="card-body">
-      <h5 class="card-title">{{ $product->name }}</h5>
-      <p class="card-text">{{ $product->price }}</p>
-      <a href="{{ route('product.show', $product->id) }}" class="btn btn-primary">Buy</a>
+  <div class="card-columns">
+    @foreach ($products as $product)
+    <div class="card" style="width: 18rem;">
+      <img src="https://via.placeholder.com/300" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">{{ $product->name }}</h5>
+        <p class="card-text">{{ $product->price }}</p>
+        <a href="{{ route('product.show', $product->id) }}" class="btn btn-primary">Buy</a>
+      </div>
     </div>
+    @endforeach
   </div>
-  @endforeach
 </div>
 @endsection
