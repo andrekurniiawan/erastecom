05c3616a4ae5bbcb32fec6b546a6d49578dde9ee
Andre Kurniawan <drekunia@gmail.com>
2021-05-19 11:20:36 +0700

    factoryseeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -21,6 +21,6 @@ class UserSeeder extends Seeder
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
-        // factory(User::class, 20)->create();
+        factory(User::class, 20)->create();
     }
 }
